eff2dd9ae587b054dd9dc194f05b8fd2120b4b61
chenxinjizhe <chenxinjizhe@prg.is.titech.ac.jp>
2019-10-23 17:48:41 +0900

    edit nbody.py pseudo code

--- python_examples/nbody.py
+++ python_examples/nbody.py
@@ -4,54 +4,117 @@ from configuration import *
 
 # cuda_block_size = 256
 
+# DynaSOArの初期化
+"""
+//何らかのシンタックスでここで使いたいクラスをすべて宣言する
+class Body;
+using AllocatorT = SoaAllocator<kNumObjects, Body>;
 
-# floatListをfloatのListとして使える
-# floatList = List[float]
+//Allocatorを宣言する
+AllocatorHandle<AllocatorT>* allocator_handle;
+__device__ AllocatorT* device_allocator;
+
+
+"""
+
 
 class Body:
-    dev_Body_pos_x: List[float]
-    # dev_Body_pos_x: floatList
-    dev_Body_pos_y: List[float]
-    dev_Body_vel_x: List[float]
-    dev_Body_vel_y: List[float]
-    dev_Body_mass: List[float]
-    dev_Body_force_x: List[float]
-    dev_Body_force_y: List[float]
-    device_checksum: List[float]
+    pos_x: float
+    pos_y: float
+    vel_x: float
+    vel_y: float
+    force_x: float
+    force_y: float
+    mass: float
+    # ここでAllocatorのFieldを呼び出す
+    """
+    declare_field_types(float, float, float, float, float, float, float, float);
+    Field<Body, 0> pos_x_;
+    Field<Body, 1> pos_y_;
+    Field<Body, 2> vel_x_;
+    Field<Body, 3> vel_y_;
+    Field<Body, 4> force_x_;
+    Field<Body, 5> force_y_;
+    Field<Body, 6> mass_;
+    """
 
-    def __init__(self, idx: int, pos_x: float, pos_y: float, vel_x: float, vel_y: float, mass: float):
-        self.dev_Body_pos_x[idx] = pos_x
-        self.dev_Body_pos_y[idx] = pos_y
-        self.dev_Body_vel_x[idx] = vel_x
-        self.dev_Body_vel_y[idx] = vel_y
-        self.dev_Body_mass[idx] = mass
+    def __init__(self, px: float, py: float, vx: float, vy: float, m: float):
+        self.pos_x = px
+        self.pos_y = py
+        self.vel_x = vx
+        self.vel_y = vy
+        self.mass = m
 
-    # device関数
-    def body_compute_force(self, idx: int):
-        self.dev_Body_force_x[idx] = 0.0
-        self.dev_Body_force_y[idx] = 0.0
-        i = 0
-        while i < kNumBodies:
-            i += 1
-            if idx != i:
-                dx: float = self.dev_Body_pos_x[i] - self.dev_Body_pos_x[idx]
-                dy: float = self.dev_Body_pos_y[i] - self.dev_Body_pos_y[idx]
-                dist: float = math.sqrt(dx * dx + dy * dy)
-                f: float = (kGravityConstant * self.dev_Body_mass[idx] * self.dev_Body_mass[i] /
-                            (dist * dist + kDampeningFactor)
-                            )
-                self.dev_Body_force_x[idx] += f * dx / dist
-                self.dev_Body_force_y[idx] += f * dy / dist
+    def compute_force(self):
+        self.force_x = 0.0
+        self.force_y = 0.0
+        # ここでdevice_doを呼び出す
+        """
+        device_allocator->template device_do<Body>(&Body::apply_force, this);
+        """
 
-    # device関数
-    def body_update(self, idx: int):
-        self.dev_Body_vel_x[idx] += self.dev_Body_force_x[idx] * kDt / self.dev_Body_mass[idx]
-        self.dev_Body_vel_y[idx] += self.dev_Body_force_y[idx] * kDt / self.dev_Body_mass[idx]
-        self.dev_Body_pos_x[idx] += self.dev_Body_vel_x[idx] * kDt
-        self.dev_Body_pos_y[idx] += self.dev_Body_vel_y[idx] * kDt
+    def apply_force(self, other: Body):
+        if other is not self:
+            dx: float = self.pos_x - other.pos_x
+            dy: float = self.pos_x - other.pos_y
+            dist: float = math.sqrt(dx * dx + dy * dy)
+            f: float = (kGravityConstant * self.mass * other.mass /
+                        (dist * dist + kDampeningFactor))
+            other.force_x += f*dx / dist
+            other.force_y += f*dy / dist
 
-        if self.dev_Body_pos_x[idx] < -1 or self.dev_Body_pos_x[idx] > 1:
-            self.dev_Body_vel_x[idx] = -self.dev_Body_vel_x[idx]
+    def body_update(self):
+        self.vel_x += self.force_x * kDt / self.mass
+        self.vel_y += self.force_y * kDt / self.mass
+        self.pos_x += self.vel_x * kDt
+        self.pos_y += self.vel_y * kDt
 
-        if self.dev_Body_pos_y[idx] < -1 or self.dev_Body_pos_y[idx] > 1:
-            self.dev_Body_vel_y[idx] = -self.dev_Body_vel_y[idx]
+        if self.pos_x < -1 or self.pos_x > 1:
+            self.vel_x = -self.vel_x
+
+        if self.pos_y < -1 or selfpos_y > 1:
+            self.vel_y = -self.vel_y
+
+    # この関数はcudaを使う必要がある
+    """
+    __global__ void kernel_initialize_bodies() {
+    int tid = threadIdx.x + blockDim.x * blockIdx.x;
+    curandState rand_state;
+    curand_init(kSeed, tid, 0, &rand_state);
+
+    for (int i = tid; i < kNumBodies; i += blockDim.x * gridDim.x) {
+        new(device_allocator) Body(
+            /*pos_x=*/ 2 * curand_uniform(&rand_state) - 1,
+            /*pos_y=*/ 2 * curand_uniform(&rand_state) - 1,
+            /*vel_x=*/ (curand_uniform(&rand_state) - 0.5) / 1000,
+            /*vel_y=*/ (curand_uniform(&rand_state) - 0.5) / 1000,
+            /*mass=*/ (curand_uniform(&rand_state)/2 + 0.5) * kMaxMass);
+    """
+    def kernel_initialize_bodies(self):
+        pass
+
+
+if __name == '__main__':
+
+    # Allocatorを作成する
+    """
+    allocator_handle = new AllocatorHandle<AllocatorT>(/*unified_memory=*/ true);
+    AllocatorT* dev_ptr = allocator_handle->device_pointer();
+    cudaMemcpyToSymbol(device_allocator, &dev_ptr, sizeof(AllocatorT*), 0,
+                        cudaMemcpyHostToDevice);
+    """
+
+    # cudaカーネルを呼び出し、Bodyのobjectを初期化する
+    """
+    kernel_initialize_bodies << < 128, 128 >> > ();
+    gpuErrchk(cudaDeviceSynchronize());
+    """
+
+    for i in range(kNumIterations):
+        # parallel_doを呼び出してforceを計算し、適用する
+        """
+        allocator_handle->parallel_do < Body, & Body::compute_force > ();
+        allocator_handle->parallel_do < Body, & Body::update > ();
+        """
+
+    # rendering part
