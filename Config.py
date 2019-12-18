9c626dc65a47a447b8e83e509032ccaebe86a18b
chenxinjizhe <jzzle15001014621@gmail.com>
2019-12-18 13:52:49 +0900

    Merge branch 'master' of github.com:prg-titech/Sanajeh
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.
    
    test pycharm git

--- Config.py
+++
@@ -1,8 +0,0 @@
-kSeed: int = 42
-kMaxMass: float = 1000.0
-kNumIterations: int = 3000
-kNumBodies: int = 30
-kDt: float = 0.02
-kGravityConstant: float = 6.673e-4
-kDampeningFactor: float = 0.05
-kNumObjects: int = 64 * 64 * 64 * 64
--- python2cpp_examples/Sample1.py
+++ python2cpp_examples/Sample1.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 from sanajeh import *
-from Config import *
 
 pAT = PyAllocatorT(kNumObjects, "Body")
 ph = PyAllocatorTHandle(pAT)
