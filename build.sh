0e738f432957801a743acb79728926d49eeb973b
chenxinjizhe <chenxinjizhe@prg.is.titech.ac.jp>
2020-05-09 17:35:40 +0900

    finish .so compiling

--- build.sh
+++ build.sh
@@ -11,4 +11,5 @@ args="${args} ${optimizations} --std=c++11 -lineinfo --expt-extended-lambda"
 args="${args} -gencode arch=compute_50,code=sm_50 -gencode arch=compute_61,code=sm_61"
 args="${args} -maxrregcount=64 -Idynasoar -I. -Idynasoar/lib/cub"
 
-nvcc --shared -Xcudafe "--diag_suppress=1427" ${args} "$@"
+nvcc --shared -Xcudafe "--diag_suppress=1427" -Xcompiler -fPIC ${args} "$@" -o bin/sanajeh_device_code.so
+
