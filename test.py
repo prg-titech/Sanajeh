808246cfbdf1f7efbd54931a7ee2d1944294d20c
chenxinjizhe <jzzle15001014621@gmail.com>
2020-06-16 15:03:01 +0900

    better output info

--- test.py
+++ test.py
@@ -24,7 +24,8 @@ for x in range(100):
     print("iteration%-3d time: %.3fs" % (x, p_do_end_time - p_do_start_time))
 end_time = time.time()
 
-print("compile time: %.3fs" % (compile_time - start_time))
-print("initialize time: %.3fs" % (initialize_time - start_time))
+print("\ncompile time: %.3fs" % (compile_time - start_time))
+print("initialize time: %.3fs" % (initialize_time - compile_time))
 print("parallel new time: %.3fs" % (parallel_new_time - initialize_time))
+print("overall computation time: %.3fs" % (end_time - parallel_new_time))
 print("overall time: %.3fs" % (end_time - start_time))
