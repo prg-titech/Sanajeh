1c1f7178ee71b255fb13b53135926cd64df36741
chenxinjizhe <jzzle15001014621@gmail.com>
2020-08-10 21:16:29 +0900

    for benchmark

--- test.py
+++ test.py
@@ -8,13 +8,13 @@ import sys
 start_time = time.perf_counter()
 
 # Compile python code to cpp code
-PyAllocator.compile(py_path='./benchmarks/nbody.py')
-compile_time = time.perf_counter()
+# PyAllocator.compile(py_path='./benchmarks/nbody.py')
+# compile_time = time.perf_counter()
 # PyAllocator.printCppAndHpp()
 # PyAllocator.printCdef()
 
 # Compile cpp code to shared library
-PyAllocator.build()
+# PyAllocator.build()
 build_time = time.perf_counter()
 
 # Load shared library and initialize device classes on GPU
@@ -29,10 +29,10 @@ parallel_new_time = time.perf_counter()
 
 # Compute on device
 for x in range(itr):
-    p_do_start_time = time.perf_counter()
+    # p_do_start_time = time.perf_counter()
     PyAllocator.parallel_do(Body, Body.compute_force)
     PyAllocator.parallel_do(Body, Body.body_update)
-    p_do_end_time = time.perf_counter()
+    # p_do_end_time = time.perf_counter()
     # print("iteration%-3d time: %.3fs" % (x, p_do_end_time - p_do_start_time))
 end_time = time.perf_counter()
 
@@ -55,11 +55,11 @@ def printAllFields(b):
 # PyAllocator.do_all(Body, printAllFields)
 end_time2 = time.perf_counter()
 
-print("compile time(py2cpp): %dµs" % ((compile_time - start_time) * 1000000))
-print("compile time(nvcc): %dµs" % ((build_time - compile_time) * 1000000))
-print("initialize time: %dµs" % ((initialize_time - build_time) * 1000000))
-print("parallel new time(%-5d objects): %.dµs" % (obn, ((parallel_new_time - initialize_time) * 1000000)))
+# print("compile time(py2cpp): %dµs" % ((compile_time - start_time) * 1000000))
+# print("compile time(nvcc): %dµs" % ((build_time - compile_time) * 1000000))
+# print("initialize time: %dµs" % ((initialize_time - build_time) * 1000000))
+# print("parallel new time(%-5d objects): %.dµs" % (obn, ((parallel_new_time - initialize_time) * 1000000)))
 print("average computation time: %dµs" % ((end_time - parallel_new_time) * 1000000 / itr))
-print("overall computation time(%-4d iterations): %dµs" % (itr, ((end_time - parallel_new_time) * 1000000)))
+# print("overall computation time(%-4d iterations): %dµs" % (itr, ((end_time - parallel_new_time) * 1000000)))
 # print("do_all time: %dµs" % ((end_time2 - end_time) * 1000000))
-print("overall time: %dµs" % ((end_time2 - start_time) * 1000000))
+# print("overall time: %dµs" % ((end_time2 - start_time) * 1000000))
