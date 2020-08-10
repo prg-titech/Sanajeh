31f74a1d165b808279ab44f8b5afb5c3fa1b51fa
chenxinjizhe <jzzle15001014621@gmail.com>
2020-08-10 18:32:15 +0900

    fxi2

--- device_code/DynaSOAr_test.cu
+++ device_code/DynaSOAr_test.cu
@@ -7,20 +7,22 @@ int main(int argc, char* argv[]) {
 #endif  // OPTION_RENDER
 
   // Create new allocator.
-	AllocatorInitialize();
+	allocator_handler = new AllocatorHandle<AllocatorT>(/* unified_memory= */ true);
+	AllocatorT* dev_ptr = allocator_handler->device_pointer();
+	cudaMemcpyToSymbol(device_allocator, &dev_ptr, sizeof(AllocatorT*), 0, cudaMemcpyHostToDevice);
 
-  allocator_handle->parallel_new<Body>(argv[1]);
+	allocator_handler->parallel_new<Body>(argv[1]);
 
 
-  for (int i = 0; i < 100; ++i) {
+	for (int i = 0; i < 100; ++i) {
 	auto time_start = std::chrono::system_clock::now();
-    allocator_handle->parallel_do<Body, &Body::compute_force>();
-    allocator_handle->parallel_do<Body, &Body::body_update>();
+	allocator_handler->parallel_do<Body, &Body::compute_force>();
+	allocator_handler->parallel_do<Body, &Body::body_update>();
 	auto time_end = std::chrono::system_clock::now();
 	auto elapsed = time_end - time_start;
 	auto micros = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
 	printf("%lu\n", micros);
-  }
+	}
 
-  return 0;
+	return 0;
 }
