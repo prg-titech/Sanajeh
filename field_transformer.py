d0e0b01aead3db7bd21cbc3f87a08e7b6c0f56ff
chenxinjizhe <jzzle15001014621@gmail.com>
2020-10-21 00:15:35 +0900

    finished the normalizer for ast.Expr nodes, Ifexp, annassign todo

--- field_transformer.py
+++ field_transformer.py
@@ -1,4 +1,5 @@
 import ast
+import astunparse
 import sys
 
 from call_graph import CallGraph
@@ -89,11 +90,14 @@ class Searcher(DeviceCodeVisitor):
 
 class Normalizer(DeviceCodeVisitor):
     """Use variables to rewrite nested expressions"""
+    # todo: nested expressions in ifexp, assign, annassign nodes
 
     def __init__(self, root: CallGraph):
         super().__init__(root)
+        self.v_counter = 0  # used to count the auto generated variables
 
     def visit_FunctionDef(self, node):
+        self.v_counter = 0  # counter needs to be reset in every function
         name = node.name
         func_node = self.node_path[-1].GetFunctionNode(name, None)
         if func_node is None:
@@ -103,11 +107,55 @@ class Normalizer(DeviceCodeVisitor):
         # If it is not a device function just skip
         if func_node.is_device:
             self.node_path.append(func_node)
-            self.visit(node.args)
-            for x in node.body:
-                self.visit(x)
+            node.args = self.visit(node.args)
+            node.body = [self.visit(x) for x in node.body]
+            i = 0
+            for x in range(len(node.body)):
+                if type(node.body[i]) == list:
+                    for n in node.body[i]:
+                        node.body.insert(i, n)
+                        i += 1
+                    del node.body[i]
+                else:
+                    i += 1
             self.node_path.pop()
         return node
+
+    def visit_Expr(self, node):
+        ret = []
+        node.value = self.visit(node.value)
+        if type(node.value) == list:
+            for v in node.value:
+                if type(v) == ast.Call:
+                    v = ast.Expr(value=v)
+                ret.append(v)
+        return ret
+
+    def visit_Call(self, node):
+        ret = []
+        if hasattr(node.func, "value") and type(node.func.value) == ast.Call:
+            assign_nodes = self.visit(node.func.value)
+            new_var_node = ast.Name(id="__auto_v" + str(self.v_counter), ctx=ast.Load())  # change argument
+            ret.extend(assign_nodes[:-1])
+            ret.append(ast.Assign(targets=[ast.Name(id="__auto_v" + str(self.v_counter), ctx=ast.Store())],
+                                  value=assign_nodes[-1]
+                                  ))
+            self.v_counter += 1
+            node.func.value = new_var_node
+        for x in range(len(node.args)):
+            if type(node.args[x]) == ast.Call:
+                if hasattr(node.args[x].func, "value") and type(node.args[x].func.value) == ast.Call:
+                    assign_nodes = self.visit(node.args[x])
+                    new_var_node = ast.Name(id="__auto_v" + str(self.v_counter), ctx=ast.Load())  # change argument
+                    ret.extend(assign_nodes[:-1])
+                    ret.append(ast.Assign(targets=[ast.Name(id="__auto_v" + str(self.v_counter), ctx=ast.Store())],
+                                          value=assign_nodes[-1]
+                                          ))
+                    self.v_counter += 1
+                    node.args[x] = new_var_node
+
+        ret.append(node)
+        return ret
 
 
 class Expander(DeviceCodeVisitor):
@@ -131,12 +179,10 @@ def transform(node, call_graph):
     for cls in scr.dev_cls:
         if cls in scr.sdef_csl:
             scr.sdef_csl.remove(cls)
-    print(scr.dev_cls, scr.sdef_csl)
 
-    ret_node = ast.fix_missing_locations(Normalizer(call_graph).visit(node))
-    # ret_node = ast.fix_missing_locations(Expander(call_graph).visit(node))
-    # ret_node = ast.fix_missing_locations(Eliminator(call_graph).visit(node))
-    return ret_node
+    ast.fix_missing_locations(Normalizer(call_graph).visit(node))
+    # ast.fix_missing_locations(Expander(call_graph).visit(node))
+    # ast.fix_missing_locations(Eliminator(call_graph).visit(node))
 
 
 tree = ast.parse(open('./benchmarks/nbody_vector.py', encoding="utf-8").read())
@@ -148,5 +194,10 @@ if not gpcgv.mark_device_data(tree):
     print("No device data found")
     sys.exit(1)
 
-new = transform(tree, gpcgv.root)
+transform(tree, gpcgv.root)
+
+path_w = './test_output.py'
+s = astunparse.unparse(tree)
+with open(path_w, mode='w') as f:
+    f.write(s)
 pass
