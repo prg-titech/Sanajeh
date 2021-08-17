f46aaf85393e62da91a9696a93db743e89056f06
Lubis Luthfan Anshar <luthfanlubis@gmail.com>
2021-08-18 01:53:25 +0900

    Added render as option

--- examples/nbody/main.py
+++ examples/nbody/main.py
@@ -14,8 +14,34 @@ parser = argparse.ArgumentParser()
 parser.add_argument("number", help="number of bodies", type=int)
 parser.add_argument("iter", help="number of iteration", type=int)
 parser.add_argument("--cpu", help="process sequentially", action="store_true")
+parser.add_argument("--r", help="rendering option", action="store_true")
 args = parser.parse_args()
 
+"""
+Rendering setting
+"""
+import pygame
+os.environ["SDL_VIDEODRIVER"] = "windib"
+
+def render(b):
+    px = int((b.pos.x + 1) * 150)
+    py = int((b.pos.y + 1) * 150)
+    pygame.draw.circle(screen, (255, 255, 255), (px, py), 2)
+
+def clear_screen():
+    screen.fill((0, 0, 0))
+
+if (args.r):
+  screen_width = 300
+  screen_height = 300
+  pygame.init()
+  screen = pygame.display.set_mode((screen_width, screen_height))
+  pygame.display.flip()  
+
+
+"""
+Main program
+"""
 allocator: PyAllocator = PyAllocator("examples/nbody/nbody.py", "nbody", args.cpu)
 
 allocator.initialize()
@@ -27,6 +53,10 @@ parallel_new_time = time.perf_counter()
 for x in range(args.iter):
   allocator.parallel_do(Body, Body.compute_force)
   allocator.parallel_do(Body, Body.body_update)
+  if (args.r):
+    allocator.do_all(Body, render)
+    pygame.display.flip()
+    clear_screen()    
 end_time = time.perf_counter()
 
 print("parallel new time(%-5d objects): %.dµs" % (args.number, ((parallel_new_time - initialize_time) * 1000000)))
