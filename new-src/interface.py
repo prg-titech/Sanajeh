940075c25473dc78e2bafcce9567fa2da46e38fe
luthfan <luthfanlubis@gmail.com>
2023-01-21 08:45:39 +0900

    Fixed interface

--- new-src/interface.py
+++ new-src/interface.py
@@ -39,12 +39,13 @@ def main():
     basename = os.path.basename(file_path)
     file_name, _ = os.path.splitext(basename)
 
+    sys.path.append(directory)
+    main_func = getattr(__import__(file_name), "main")
+
     if run_cpu:
         main_func(sanajeh.SeqAllocator(), to_render)
         return
     elif to_run:
-        sys.path.append(directory)
-        main_func = getattr(__import__(file_name), "main")
         main_func(sanajeh.PyAllocator(file_name), to_render)
         return
 
